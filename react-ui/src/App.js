110413c65aa5d854a9c436d5e043f5cb55db8c1e
ImperialBreakCode <eeeda522@gmail.com>
2022-03-25 19:02:32 +0200

    first

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,6 +1,16 @@
+import './styles/style.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-  return null;
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <h1 className='title-app'>Language app</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
