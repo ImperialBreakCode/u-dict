0ff63cf2ea542cac45aaa067431faf8cbcfb9684
ImperialBreakCode <eeeda522@gmail.com>
2022-04-07 12:30:18 +0300

    home view save 2

--- react-ui/src/views/home.jsx
+++ react-ui/src/views/home.jsx
@@ -14,13 +14,13 @@ class HomeView extends React.Component{
 
                     <div className="row">
                         <div className="col-xl-4">
-                            <Card></Card>
+                            <Card title='languages' data='10' />
                         </div>
                         <div className="col-xl-4">
-                            <h4>words</h4>
+                            <Card title='words' data='10' />
                         </div>
                         <div className="col-xl-4">
-                            <h4>phrases</h4>
+                            <Card title='phrases' data='10' />
                         </div>
                     </div>
                 </div>
@@ -32,11 +32,10 @@ class HomeView extends React.Component{
 
 function Card(props) {
     return(
-        <div className="card dash-card" style={{width: 19 + 'rem'}}>
+        <div className="card dash-card">
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 className="card-title">{props.title}</h5>
+                <h3 className="mb-2">{props.data}</h3>
             </div>
         </div>
     );
